44a3c63af51d4ae7905d46bb46e34be3e51eeb59
Coco Zhao <35900015+cocozhao321@users.noreply.github.com>
2021-02-01 21:11:22 -0500

    Add files via upload
    Allow bidsStreamer to support dicom or dicom directory

--- OpenNeuroSample/streamer_test.py
+++ OpenNeuroSample/streamer_test.py
@@ -7,43 +7,22 @@ carefully designed.
 """
 
 from rtCommon.bidsArchive import BidsArchive
-from rtCommon.bidsIncremental import BidsIncremental
 import time
 from projects.OpenNeuroSample.OpenNeuroProto import OpenNeuroOverview
-from projects.OpenNeuroSample.initialize import OutputUpdate
-import nibabel as nib
-# For the streamer, we might return an instance of something like this class
-# (with specified characteristics, like subject and session) whenever an
-# iteration is requested. That way, the streamer doesn't have to maintain
-# internal state about the status of various iterators, it's fully encapsulated
-# in this class.
-#
-# As an example of a case where maintaining the internal state of the iterator
-# in the streamer becomes difficult, a user may first request streaming subject
-# 1 session 1, then break halfway through the iteration after getting the first
-# half of the session, and then switch to subject 2 session 1, and break after
-# getting the first half of that session, and then compare the two.
-#
-# In such a case, we wouldn't want the streamer having to figure out when it's
-# supposed to reset the internal state of the various iterators; rather, it
-# should return two, distinct and independent iterators for use by the client
-# for those two distinct requests.
-#
-# This also avoids the weird logic currently present around setting
-# _stream_empty when the iterator runs out the first time, and returning None.
-# In the 'return an iterator' implementation, this won't be a concern.
+from projects.OpenNeuroSample.initialize import OpenNeuroUpdate
+from rtCommon.dicomToBidsService import dicomToBidsinc, dicomDirToBidsinc
+
 class bidsStreamer():
 
-    def __init__(self, dataset, datasetType):
+    def __init__(self, dataset, datasetType, sliceIndex:int = -1):
         self.dataset = dataset
-        self._index = -1
+        self.index = sliceIndex
         self.type = datasetType
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        self._index += 1
+    def get_next_image(self, index:int = 0):
+        if index != 0:
+            self.index = index - 1
+        self.index += 1
         if self.type == 'archive':
             # Subject: 001
             # Session: 01
@@ -51,53 +30,81 @@ class bidsStreamer():
             # Run: 01
             # Suffix: Bold
             NUM_TIMEPOINTS = 196  # 4th dimension of NIfTI image -- manually extracted
-            if self._index >= NUM_TIMEPOINTS:
+            if self.index >= NUM_TIMEPOINTS:
                 raise StopIteration
             else:
                 return self.dataset.getIncremental(
                     subject='04',
                     session='01',
-                    task='flanker',
+                    task='task-flanker',
                     suffix='bold',
-                    dataType='func',
-                    sliceIndex=self._index,
+                    datatype='func',
+                    sliceIndex=self.index,
                     otherLabels={'run': 1})
         elif self.type == 'openNeuro':
-            NUM_TIMEPOINTS = self.dataset.dataset_Bidsinc(sliceIndex=self._index)[1]
-            if self._index >= NUM_TIMEPOINTS:
+            NUM_TIMEPOINTS = self.dataset.dataset_Bidsinc(sliceIndex=self.index)[1]
+            if self.index >= NUM_TIMEPOINTS:
                 raise StopIteration
             else:
-                return self.dataset.dataset_Bidsinc(sliceIndex=self._index)[0]
-    # This will need some work if it's to be exposed externally, as the
-    # StopIteration exception won't automatically trigger halt iteration, like
-    # when this is used in the 'for image in archiveIterator:' usage.
-    #get_next_image = __next__
+                return self.dataset.dataset_Bidsinc(sliceIndex=self.index)[0]
+        elif self.type == 'dicom':
+            return self.dataset
+        elif self.type == 'dicomDir':
+            return self.dataset[self.index]
 
+def dicomMetadataSample() -> dict:
+    sample = {}
+    sample["ContentDate"] = "20190219"
+    sample["ContentTime"] = "124758.653000"
+    sample["RepetitionTime"] = 1500
+    sample["StudyDescription"] = "Norman_Mennen^5516_greenEyes"
+    sample["StudyInstanceUID"] = \
+        "1.3.12.2.1107.5.2.19.45031.30000019021215313940500000046"
+    sample["ProtocolName"] = "func_ses-01_task-faces_run-01"
+    return sample
 
-def main():
-    #OpenNeuro Example
-    overview = OpenNeuroOverview()  # initialize OpenNeuroOverview
-    print(overview.display_description("ds000102"))  # check description before download
-    print(overview.display_readme("ds000102"))  # check readme file (if exists) before download
-    # download a subject's data only and reformat it as BIDS-I
-    download_dataset = overview.get_dataset("ds000102", False, "04")  # get dataset based on accession number and id
-    openneuroDataset = OutputUpdate(download_dataset)
-    openneuroDataset.webCommunication() #  communicate with web-platform and update conf file
-    openStream = bidsStreamer(openneuroDataset,'openNeuro')
-    for incremental in openStream:
-        print(incremental)
-        time.sleep(.1)
-
-        
+def main(args):
+    #args = sys.argv[1:]
+    if args == "openneuro":
+        #OpenNeuro Example
+        overview = OpenNeuroOverview()  # initialize OpenNeuroOverview
+        print(overview.display_description("ds000102"))  # check description before download
+        print(overview.display_readme("ds000102"))  # check readme file (if exists) before download
+        # download a subject's data only and reformat it as BIDS-I
+        download_dataset = overview.get_dataset("ds000102", False, "04")  # get dataset based on accession number and id
+        openneuroDataset = OpenNeuroUpdate(download_dataset)
+        openneuroDataset.webCommunication() #  communicate with web-platform and update conf file
+        openStream = bidsStreamer(openneuroDataset,'openNeuro')
+        #print(openStream.get_next_image(110))
+        for i in range(10):
+            print(openStream.get_next_image())
+            time.sleep(.1)
     # bidsArchive example
-    """
-    path = '/Users/cocozhao/Desktop/rt-cloud-bidsinc-dev/projects/OpenNeuroSample'
-    archiveDataset = BidsArchive(path)
-    archivestream = bidsStreamer(archiveDataset,'archive')
-    
-    for incremental in archivestream:
-        print(incremental)
-        time.sleep(.2)
-    """
+    elif args == "bidsArchive":
+        path = '/Users/cocozhao/Desktop/rt-cloud-bidsinc-dev/projects/OpenNeuroSample/ds000102'
+        archiveDataset = BidsArchive(path)
+        archivestream = bidsStreamer(archiveDataset,'archive')
+        #print(archivestream.get_next_image(1))
+        for i in range(196):
+            print(archivestream.get_next_image(i))
+            time.sleep(.1)
+    elif args == "dicomDir":
+        dicomDir = "/Users/cocozhao/Desktop/rt-cloud-bidsinc-dev/projects/sample/dicomDir/20190219.0219191_faceMatching.0219191_faceMatching"
+        requiredMetadata = {'subject': '01', 'task': 'faces', 'suffix': 'bold',  # REQUIRED
+            'session': '01', 'run': 1}
+        TR = 10
+        dicomDirDataset = dicomDirToBidsinc(dicomDir,TR, requiredMetadata)
+        dicomDirStream = bidsStreamer(dicomDirDataset,'dicomDir')
+        for i in range(TR):
+            print(dicomDirStream.get_next_image())
+    elif args == "dicom":
+        requiredMetadata = {'subject': '01', 'task': 'faces', 'suffix': 'bold',  # REQUIRED
+                            'session': '01', 'run': 1}
+        dicomFile = "/Users/cocozhao/Desktop/rt-cloud-bidsinc-dev/projects/sample/dicomDir/" \
+                    "20190219.0219191_faceMatching.0219191_faceMatching/001_000013_000005.dcm"
+        dicomDataset = dicomToBidsinc(dicomFile,requiredMetadata)
+        dicomStream = bidsStreamer(dicomDataset,'dicom')
+        print(dicomStream.get_next_image())
+
 if __name__ == "__main__":
-    main()
+    main('dicomDir')
